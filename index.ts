7f8a2528fa3e4825522efa049c200bbb5efa12ff
pflofif <bogdanvivchar8@gmail.com>
2023-02-27 23:33:45 +0200

    finish 2 task : all permutation of word

--- index.ts
+++ index.ts
@@ -3,7 +3,29 @@ class Lab1Functions {
     let names: Array<string> = inputStr.split(" ");
     console.log(names);
   }
+  AllPermutations(word: string) {
+    console.log(word);
+    let charArray = word.split("");
+    const n = charArray.length;
+    const p = charArray.map((_, i) => i);
+
+    let i = 1;
+    while (i < n) {
+      p[i]--;
+
+      let j = (i % 2) * p[i];
+      [charArray[j], charArray[i]] = [charArray[i], charArray[j]];
+      console.log(charArray.join(""));
+
+      i = 1;
+      while (p[i] == 0) {
+        p[i] = i;
+        i += 1;
+      }
+    }
+  }
 }
 
 let laba = new Lab1Functions();
 laba.splitStringByWords("Bohdan Anton Somethin And More");
+laba.AllPermutations("Bohdan");
